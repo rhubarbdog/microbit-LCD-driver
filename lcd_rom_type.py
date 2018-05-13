b6e3f379020bdd9467c99855c1cf10d857932efa
rhubarbdog <33667064+rhubarbdog@users.noreply.github.com>
2018-05-13 14:20:50 +0000

    A program to determine the character set of an LCD display

---
+++ lcd_rom_type.py
@@ -0,0 +1,19 @@
+# requires RPi_I2C_driver.py
+from microbit import *
+import microbit_i2c_lcd as lcd
+
+i2c.init(sda=pin15,scl=pin13)
+
+display = lcd.lcd(i2c)
+
+#
+# this will print a pi symbol for ROM A00 japaneese
+# print a divide symbol for the A02 ROM european
+
+message = str(chr(247))+" is pi : Japan"
+display.lcd_display_string(message, 1)
+message = str(chr(247))+" is div : Euro"
+display.lcd_display_string(message, 2)
+
+
+i2c.init(sda=pin20,scl=pin19)
